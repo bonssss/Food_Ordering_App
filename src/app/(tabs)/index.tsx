a2a81d8ae08f68c31de755547b038439a90338a7
Binary Cosmo <binarycosmo682@gmail.com>
2025-01-23 15:13:58 +0300

    First custom componenet

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,46 +1,17 @@
 import { StyleSheet,Image } from 'react-native';
 
-import EditScreenInfo from '@/src/components/EditScreenInfo';
 import { Text, View } from '@/src/components/Themed';
-import Colors from '@/src/constants/Colors';
-import products from '@/assets/data/products';
 
-const product = products[0]
+import products from '@/assets/data/products';
+import ProductListItem from '@/src/components/ProductListItem';
+
+
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={{uri:product.image}}/>
-
-     <Text style={styles.title}>{product.name}</Text>
-     <Text style={styles.price}>${product.price}</Text>
-
-    </View>
+    <View >
+    <ProductListItem  product   ={products[2]}/>
+    <ProductListItem   product   ={products[5]}/>
+  </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor:'white',
-    padding:10,
-    borderRadius:20
-  
-  },
-  image:{
-    width:'100%',
-    aspectRatio:1
-  
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    marginVertical:10,
-    color: Colors.dark.background
-
-  },
-  price:{
-    fontSize: 20,
-    color: Colors.light.tint
-  }
- 
-});
---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,46 @@
+import { StyleSheet,Image } from 'react-native';
+
+import { Text, View } from '@/src/components/Themed';
+import Colors from '@/src/constants/Colors';
+import products from '@/assets/data/products';
+
+
+const ProductListItem = ({product}) =>{
+  return(
+    <View style={styles.container}>
+    <Image style={styles.image} source={{uri:product.image}}/>
+
+   <Text style={styles.title}>{product.name}</Text>
+   <Text style={styles.price}>${product.price}</Text>
+
+  </View>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor:'white',
+    padding:10,
+    borderRadius:20
+  
+  },
+  image:{
+    width:'100%',
+    aspectRatio:1
+  
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginVertical:10,
+    color: Colors.dark.background
+
+  },
+  price:{
+    fontSize: 20,
+    color: Colors.light.tint
+  }
+ 
+});
