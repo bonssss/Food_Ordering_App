f4ee0adbd91ae3a0634d562bef3e4588521058a5
Binary Cosmo <bons6710hos@gmail.com>
2025-02-14 11:18:07 +0300

    finalize the auth by protecting routes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,42 +1,42 @@
-import { View, Text, ActivityIndicator } from 'react-native';
-import React from 'react';
-import Button from '../components/Button';
-import { Link, Redirect } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { useAuth } from '../providers/AuthProvider';
-import { supabase } from '../lib/supabase';
+import { View, Text, ActivityIndicator } from "react-native";
+import React from "react";
+import Button from "../components/Button";
+import { Link, Redirect } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useAuth } from "../providers/AuthProvider";
+import { supabase } from "../lib/supabase";
 
 const index = () => {
-
-  const {session,loading} =useAuth();
+  const { session, loading,isAdmin } = useAuth();
   // console.log(session);
-  if (loading){
-    return <ActivityIndicator/>
+  if (loading) {
+    return <ActivityIndicator />;
   }
 
-  if(!session){
-    return <Redirect href={'/sign-in'}/>
-    
+  if (!session) {
+    return <Redirect href={"/sign-in"} />;
+  }
+
+  if (!isAdmin){
+    return <Redirect href={"/(user)"} />;
+
 
   }
-  
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
-      <Link href={'/(user)'} asChild>
+    <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
+      <Link href={"/(user)"} asChild>
         <Button text="User" />
       </Link>
-      <Link href={'/(admin)'} asChild>
+      <Link href={"/(admin)"} asChild>
         <Button text="Admin" />
       </Link>
-      <Link href={'/sign-in'} asChild>
-  <Button text="Sign in" />
-</Link>
-  <Button onPress={()=> supabase.auth.signOut()} text="Sign out" />
+      {/* <Link href={"/sign-in"} asChild>
+        <Button text="Sign in" />
+      </Link> */}
+      <Button onPress={() => supabase.auth.signOut()} text="Sign out" />
 
-
-
-
-<StatusBar style="dark" backgroundColor="#4f3" />
+      <StatusBar style="dark" backgroundColor="#4f3" />
     </View>
   );
 };
