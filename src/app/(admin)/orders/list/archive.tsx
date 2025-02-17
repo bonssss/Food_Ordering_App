8443b4c5fcdf3812800e17c6aba9bbd49d59857e
bons <bons6710hos@gmail.com>
2025-02-17 21:43:31 +0300

    read orders

--- src/app/(admin)/orders/list/archive.tsx
+++ src/app/(admin)/orders/list/archive.tsx
@@ -1,12 +1,22 @@
-import { FlatList } from 'react-native';
-import orders from '../../../../../assets/data/orders';
-import OrderListItem from '../.././../../components/OrderListItem';
-import { Stack } from 'expo-router';
-
+import { ActivityIndicator, FlatList } from "react-native";
+import orders from "../../../../../assets/data/orders";
+import OrderListItem from "../.././../../components/OrderListItem";
+import { Stack } from "expo-router";
+import { useOrderList } from "@/src/api/orders";
+import { Text } from "react-native";
 export default function OrdersScreen() {
+  const { data: orders, error, isLoading } = useOrderList({ archived: true });
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Failed to fetch</Text>;
+  }
+
   return (
     <>
-      <Stack.Screen options={{ title: 'Orders' }} />
+      <Stack.Screen options={{ title: "Orders" }} />
       <FlatList
         data={orders}
         contentContainerStyle={{ gap: 10, padding: 10 }}
--- src/app/(user)/orders/index.tsx
+++ src/app/(user)/orders/index.tsx
@@ -1,12 +1,22 @@
-import { FlatList } from 'react-native';
-import orders from '../../../../assets/data/orders';
-import OrderListItem from '../../../components/OrderListItem';
-import { Stack } from 'expo-router';
+import { ActivityIndicator, FlatList } from "react-native";
+import OrderListItem from "../../../components/OrderListItem";
+import { Stack } from "expo-router";
+import { useMyOrders, useOrderList } from "@/src/api/orders";
+import { Text } from "react-native";
 
 export default function OrdersScreen() {
+  const { data: orders, error, isLoading } = useMyOrders();
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Failed to fetch</Text>;
+  }
+
   return (
     <>
-      <Stack.Screen options={{ title: 'Orders' }} />
+      <Stack.Screen options={{ title: "Orders" }} />
       <FlatList
         data={orders}
         contentContainerStyle={{ gap: 10, padding: 10 }}
